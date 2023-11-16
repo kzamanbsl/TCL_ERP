4a7b03a0378dadcc0a669b05aa979473bc902aa1
atkhssn <atik.hassan@outlook.com>
2023-11-16 15:10:38 +0600

    Bill requisition item with unit id added

--- KGERP/Controllers/BillRequisitionController.cs
+++ KGERP/Controllers/BillRequisitionController.cs
@@ -20,9 +20,12 @@ namespace KGERP.Controllers
     public class BillRequisitionController : BaseController
     {
         private readonly IBillRequisitionService _service;
-        public BillRequisitionController(IBillRequisitionService billRequisitionService)
+        private readonly ConfigurationService _configurationService;
+
+        public BillRequisitionController(IBillRequisitionService billRequisitionService, ConfigurationService configurationService)
         {
             _service = billRequisitionService;
+            _configurationService = configurationService;
         }
 
         #region BoQ Item
@@ -72,6 +75,7 @@ namespace KGERP.Controllers
             var viewData = new BillRequisitionItemModel()
             {
                 CompanyFK = companyId,
+                Units = _service.GetUnitList(companyId),
                 BillRequisitionItems = _service.GetBillRequisitionItemList()
             };
             return View(viewData);
