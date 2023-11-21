2ee08fb9c842c4f87e465095bcf1de743bb3d1cb
shakil mahmud <shakilmahmud012@gmail.com>
2023-11-21 18:15:19 +0600

    Total Added in the List

--- KGERP.Service/Implementation/BillRequisitionService.cs
+++ KGERP.Service/Implementation/BillRequisitionService.cs
@@ -916,46 +916,102 @@ namespace KGERP.Service.Implementation
                                           }).FirstOrDefault());
             return v;
         }
+        //public async Task<BillRequisitionMasterModel> GetBillRequisitionMasterList(int companyId, DateTime? fromDate, DateTime? toDate, int? statusId)
+        //{
+        //    BillRequisitionMasterModel BillRequisitionMasterModel = new BillRequisitionMasterModel();
+        //    BillRequisitionMasterModel.CompanyFK = companyId;
+        //    BillRequisitionMasterModel.DataList = await Task.Run(() => (from t1 in _context.BillRequisitionMasters.Where(x => x.IsActive
+        //                                                  && x.CompanyId == companyId)
+        //                                                                join t2 in _context.Accounting_CostCenter on t1.CostCenterId equals t2.CostCenterId into t2_Join
+        //                                                                from t2 in t2_Join.DefaultIfEmpty()
+        //                                                                join t3 in _context.BillRequisitionTypes on t1.BillRequisitionTypeId equals t3.BillRequisitionTypeId into t3_Join
+        //                                                                from t3 in t3_Join.DefaultIfEmpty()
+        //                                                                join t4 in _context.Accounting_CostCenterType on t1.ProjectTypeId equals t4.CostCenterTypeId into t4_Join
+        //                                                                from t4 in t4_Join.DefaultIfEmpty()
+        //                                                                select new BillRequisitionMasterModel
+        //                                                                {
+        //                                                                    BillRequisitionMasterId = t1.BillRequisitionMasterId,
+        //                                                                    BillRequisitionTypeId = t1.BillRequisitionTypeId,
+        //                                                                    BOQItemId = t1.BOQItemId,
+        //                                                                    ProjectTypeId = t1.ProjectTypeId,
+        //                                                                    ProjectTypeName = t4.Name,
+        //                                                                    BRTypeName = t3.Name,
+        //                                                                    CostCenterId = t1.CostCenterId,
+        //                                                                    CostCenterName = t2.Name,
+        //                                                                    Description = t1.Description,
+        //                                                                    BRDate = t1.BRDate,
+        //                                                                    BillRequisitionNo = t1.BillRequisitionNo,
+        //                                                                    StatusId = (EnumBillRequisitionStatus)t1.StatusId,
+        //                                                                    CompanyFK = t1.CompanyId,
+        //                                                                    CreatedDate = t1.CreateDate,
+        //                                                                    CreatedBy = t1.CreatedBy,
+        //                                                                }).OrderByDescending(x => x.BillRequisitionMasterId).AsEnumerable());
+
+        //    if (statusId != -1 && statusId != null)
+        //    {
+        //        BillRequisitionMasterModel.DataList = BillRequisitionMasterModel.DataList.Where(q => q.StatusId == (EnumBillRequisitionStatus)statusId);
+        //    }
+        //    var masterIds = BillRequisitionMasterModel.DataList.Select(x => x.BillRequisitionMasterId);
+
+        //    return BillRequisitionMasterModel;
+        //}
         public async Task<BillRequisitionMasterModel> GetBillRequisitionMasterList(int companyId, DateTime? fromDate, DateTime? toDate, int? statusId)
         {
             BillRequisitionMasterModel BillRequisitionMasterModel = new BillRequisitionMasterModel();
             BillRequisitionMasterModel.CompanyFK = companyId;
-            BillRequisitionMasterModel.DataList = await Task.Run(() => (from t1 in _context.BillRequisitionMasters.Where(x => x.IsActive
-                                                          && x.CompanyId == companyId)
-                                                                        join t2 in _context.Accounting_CostCenter on t1.CostCenterId equals t2.CostCenterId into t2_Join
-                                                                        from t2 in t2_Join.DefaultIfEmpty()
-                                                                        join t3 in _context.BillRequisitionTypes on t1.BillRequisitionTypeId equals t3.BillRequisitionTypeId into t3_Join
-                                                                        from t3 in t3_Join.DefaultIfEmpty()
-                                                                        join t4 in _context.Accounting_CostCenterType on t1.ProjectTypeId equals t4.CostCenterTypeId into t4_Join
-                                                                        from t4 in t4_Join.DefaultIfEmpty()
-                                                                        select new BillRequisitionMasterModel
-                                                                        {
-                                                                            BillRequisitionMasterId = t1.BillRequisitionMasterId,
-                                                                            BillRequisitionTypeId = t1.BillRequisitionTypeId,
-                                                                            BOQItemId = t1.BOQItemId,
-                                                                            ProjectTypeId = t1.ProjectTypeId,
-                                                                            ProjectTypeName = t4.Name,
-                                                                            BRTypeName = t3.Name,
-                                                                            CostCenterId = t1.CostCenterId,
-                                                                            CostCenterName = t2.Name,
-                                                                            Description = t1.Description,
-                                                                            BRDate = t1.BRDate,
-                                                                            BillRequisitionNo = t1.BillRequisitionNo,
-                                                                            StatusId = (EnumBillRequisitionStatus)t1.StatusId,
-                                                                            CompanyFK = t1.CompanyId,
-                                                                            CreatedDate = t1.CreateDate,
-                                                                            CreatedBy = t1.CreatedBy,
-                                                                        }).OrderByDescending(x => x.BillRequisitionMasterId).AsEnumerable());
+            var dataQuery = (from t1 in _context.BillRequisitionMasters
+                             where t1.IsActive && t1.CompanyId == companyId
+                             join t2 in _context.Accounting_CostCenter on t1.CostCenterId equals t2.CostCenterId into t2_Join
+                             from t2 in t2_Join.DefaultIfEmpty()
+                             join t3 in _context.BillRequisitionTypes on t1.BillRequisitionTypeId equals t3.BillRequisitionTypeId into t3_Join
+                             from t3 in t3_Join.DefaultIfEmpty()
+                             join t4 in _context.Accounting_CostCenterType on t1.ProjectTypeId equals t4.CostCenterTypeId into t4_Join
+                             from t4 in t4_Join.DefaultIfEmpty()
+                             select new BillRequisitionMasterModel
+                             {
+                                 BillRequisitionMasterId = t1.BillRequisitionMasterId,
+                                 BillRequisitionTypeId = t1.BillRequisitionTypeId,
+                                 BOQItemId = t1.BOQItemId,
+                                 ProjectTypeId = t1.ProjectTypeId,
+                                 ProjectTypeName = t4.Name,
+                                 BRTypeName = t3.Name,
+                                 CostCenterId = t1.CostCenterId,
+                                 CostCenterName = t2.Name,
+                                 Description = t1.Description,
+                                 BRDate = t1.BRDate,
+                                 BillRequisitionNo = t1.BillRequisitionNo,
+                                 StatusId = (EnumBillRequisitionStatus)t1.StatusId,
+                                 CompanyFK = t1.CompanyId,
+                                 CreatedDate = t1.CreateDate,
+                                 CreatedBy = t1.CreatedBy,
+                             }).OrderByDescending(x => x.BillRequisitionMasterId).AsEnumerable();
 
             if (statusId != -1 && statusId != null)
             {
-                BillRequisitionMasterModel.DataList = BillRequisitionMasterModel.DataList.Where(q => q.StatusId == (EnumBillRequisitionStatus)statusId);
+                dataQuery = dataQuery.Where(q => q.StatusId == (EnumBillRequisitionStatus)statusId);
             }
+
+            BillRequisitionMasterModel.DataList = await Task.Run(() => dataQuery.ToList());
+
+            var masterIds = BillRequisitionMasterModel.DataList.Select(x => x.BillRequisitionMasterId);
+
+            var matchingDetails = await _context.BillRequisitionDetails
+                                        .Where(detail => masterIds.Contains(detail.BillRequisitionMasterId))
+                                        .ToListAsync();
+
+            foreach (var master in BillRequisitionMasterModel.DataList)
+            {
+                var detailsForMaster = matchingDetails.Where(detail => detail.BillRequisitionMasterId == master.BillRequisitionMasterId);
+                decimal total = detailsForMaster.Sum(detail => detail.UnitRate * detail.DemandQty);
+                master.TotalAmount = total;
+            }
+
             return BillRequisitionMasterModel;
         }
 
+
         #endregion
-      
+
         #region 1.2 BillRequisition Approve circle
 
         public async Task<long> PMBillRequisitionApproved(BillRequisitionMasterModel billRequisitionMasterModel)
