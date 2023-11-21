edbc3b2898ad0538d8f0d3121dc8011344a5b03d
atkhssn <atik.hassan@outlook.com>
2023-11-21 14:44:02 +0600

    Bill requisiution BoQ map UI implemented

---
+++ KGERP.Service/ServiceModel/BillRequisitionItemBoQMapModel.cs
@@ -0,0 +1,21 @@
+﻿using KGERP.Data.Models;
+using KGERP.Service.Implementation.Configuration;
+using KGERP.Utility;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace KGERP.Service.ServiceModel
+{
+    public class BillRequisitionItemBoQMapModel : BaseVM
+    {
+        public int BoQItemId { get; set; }
+        public int BillRequisitionItemId { get; set; }
+        public int EstimateQuantity { get; set; }
+        public decimal EstimateAmount { get; set; }
+
+        public List<BillBoQItem> BillBoQItems { get; set; }
+        public List<BillRequisitionItem> BillRequisitionItems { get; set; }
+    }
+}
