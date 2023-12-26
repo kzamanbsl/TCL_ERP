dc0fa05974a7192437450eeeb6535657d7167bcf
atkhssn <atik.hassan@outlook.com>
2023-12-26 14:17:59 +0600

    Bill requisition master and detail page bug fixed

--- KGERP.Service/Implementation/BillRequisitionService.cs
+++ KGERP.Service/Implementation/BillRequisitionService.cs
@@ -880,7 +880,7 @@ namespace KGERP.Service.Implementation
 
                                                                join t2 in _context.Accounting_CostCenter on t1.CostCenterId equals t2.CostCenterId into t2_Join
                                                                from t2 in t2_Join.DefaultIfEmpty()
-                                                               join t3 in _context.BillRequisitionTypes on t1.BillRequisitionTypeId equals t3.BillRequisitionTypeId into t3_Join
+                                                               join t3 in _context.ProductCategories on t1.BillRequisitionTypeId equals t3.ProductCategoryId into t3_Join
                                                                from t3 in t3_Join.DefaultIfEmpty()
                                                                join t4 in _context.Accounting_CostCenterType on t1.ProjectTypeId equals t4.CostCenterTypeId into t4_Join
                                                                from t4 in t4_Join.DefaultIfEmpty()
