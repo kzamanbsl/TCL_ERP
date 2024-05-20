b7d7b5ced0352ee9f6708710908eeb39e00b2d23
atkhssn <atik.hassan@outlook.com>
2024-05-20 17:21:08 +0600

    Product Category Bulk Upload

---
+++ KGERP.Service/ServiceModel/BulkUpload.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+
+namespace KGERP.Service.ServiceModel
+{
+    public class BulkUpload
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CompanyId { get; set; }
+        public HttpPostedFileBase FormFile { get; set; }
+    }
+}
---
+++ KGERP/Controllers/BulkUploadController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using KGERP.Service.Implementation;
+using KGERP.Service.Implementation.FTP;
+using KGERP.Service.Interface;
+using KGERP.Service.ServiceModel;
+using KGERP.Utility;
+
+namespace KGERP.Controllers
+{
+    [SessionExpire]
+    public class BulkUploadController : Controller
+    {
+        private BulkUploadService _service;
+        public BulkUploadController(BulkUploadService bulkUploadService)
+        {
+            this._service = bulkUploadService;
+        }
+
+        [HttpGet]
+        public ActionResult MaterialCategoryUpload(int companyId = 0)
+        {
+            BulkUpload viewModel = new BulkUpload();
+            viewModel.CompanyId = companyId;
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult MaterialCategoryUpload(BulkUpload model)
+        {
+            if (model.FormFile != null)
+            {
+                var response = _service.ProductCategoryUploadAsync(model);
+            }
+            return RedirectToAction(nameof(MaterialCategoryUpload), new { companyId = 21 });
+        }
+    }
+}
